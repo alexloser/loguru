5f727e5ff388850fdc0fecbb933feff996a1a499
alexloser <cypro666@gmail.com>
2024-12-23 17:35:59 +0800

    update

--- build.py
+++ build.py
@@ -1,8 +1,6 @@
 #!python3
 import sys, os
 import py_compile
-from setuptools import setup
-from Cython.Build import cythonize
 from pymagic import (
     file_exist,
     make_directory,
@@ -17,8 +15,8 @@ PROJECTS = DISKROOT + "/PythonProjects"
 
 RELEASE = f"{DISKROOT}/Release/loguru"
 
-
-DLL_LIST = [
+PYC_LIST = [
+    "loguru/__init__.py",
     "loguru/_asyncio_loop.py",
     "loguru/_better_exceptions.py",
     "loguru/_colorama.py",
@@ -36,10 +34,6 @@ DLL_LIST = [
     "loguru/_recattrs.py",
     "loguru/_simple_sinks.py",
     "loguru/_string_parsers.py",
-]
-
-PYC_LIST = [
-    "loguru/__init__.py",
     "loguru/_logger.py",
 ]
 
@@ -55,12 +49,9 @@ def compile_source():
     make_directory(RELEASE)
     sys.argv.append("build")
 
-    setup(ext_modules=cythonize(DLL_LIST, language_level=3, build_dir="build"), zip_safe=False)
-
-    copy_tree("build/lib.win-amd64-cpython-312/loguru", RELEASE)
     for name in PYC_LIST:
         cname = f'{RELEASE}/{base_name(name).replace(".py", ".pyc")}'
-        py_compile.compile(name, cfile=cname, optimize=2)
+        print(py_compile.compile(name, cfile=cname, optimize=2))
 
 
 if __name__ == "__main__":
